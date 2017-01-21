46c30c21271378e0dfc8da595787a62126312365
Michael Hawes <mhawes24@gmail.com>
2017-01-21 11:45:49 -0500

    added form changing, input extraction, and generic function

--- app.py
+++ app.py
@@ -20,26 +20,71 @@ note4 = "but they could rock you in PlayStation"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    try:
+        if request.method == "POST":
 
+            session['genre'] = request.form['genre']
+            print(session['genre'])
+            
+            genre = session['genre']
+            if genre.lower() == "edm":
+                pass
+                # retrieve lyrics from database and pass to lyrics()
+                return redirect(url_for('lyrics'))
+
+            elif genre.lower() == "classical":
+                pass
+                return redirect(url_for('lyrics'))
+
+            elif genre.lower() == "pop":
+                pass
+                return redirect(url_for('lyrics'))
+
+            else:
+                raise ValueError('No lyrics matching your request were found')
+                return redirect(url_for('index'))
+
+            return redirect(url_for('lyrics'))
+        return render_template('index.html')
+    except:
+        raise TypeError('could not process operation')
+
+
+
+
+@app.route('/lyrics', methods=['GET', 'POST'])
+def lyrics():
     listy = [note1,note2,note3,note4]
     if request.method == "POST":
         session['message'] = request.form['message1']  # get search text
         session['message'] += " " + request.form['message2']
         session['message'] += " " + request.form['message3']
         session['message'] += " " + request.form['message4']
-        session['genre'] = request.form['genre']
-        print(session['message'])
-        print(session['genre'])
-        path = './lyric_content/'+str(session['genre'])+'.txt'
-        if os.path.exists(path):
-            filevar = open(path, 'a')
-        else:
-            filevar = open(path, 'w')
-        filevar.write(str(session['message']))
-        filevar.write("\n")
-        filevar.close()
-        return redirect(url_for('index'))
-    return render_template('index.html', results=listy)
+        song_lyrics = session['message']
+        print(song_lyrics)
+
+        # if os.path.exists(path):
+        #     filevar = open(path, 'a')
+        # else:
+        #     filevar = open(path, 'w')
+        # filevar.write(str(session['message']))
+        # filevar.write("\n")
+        # filevar.close()
+        return redirect(url_for('lyrics'))
+    return render_template('lyrics.html', results=listy)
+
+
+def retrieve_lyrics(genre,song_lyrics):
+    path = './lyric_content/'+str(genre)+'.txt'
+    if os.path.exists(path):
+        filevar = open(path, 'a')
+    else:
+        filevar = open(path, 'w')
+    filevar.write(str(song_lyrics))
+    filevar.write("\n")
+    filevar.close()
+
+
 
 
 if __name__ == '__main__':
