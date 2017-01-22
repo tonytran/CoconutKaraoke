4e564b9685f8732d83a6a7952ceeb9dfb5e88b0a
Kevin Brown <kevin@kevin-brown.com>
2017-01-22 01:26:44 -0500

    Generate new lyrics based on the existing data set

--- lyrics_markov.py
+++ lyrics_markov.py
@@ -1,5 +1,6 @@
-from markov import markov_chainify, markov_dictify
+from markov import markov_chainify, markov_dictify, START, END
 import glob
+import random
 
 
 lyrics_dir = '_data/lyric_genre/rock/'
@@ -30,4 +31,30 @@ for song_path in glob.glob(lyrics_dir + '*.txt'):
 
     all_chains.extend(line_chains)
 
-print(markov_dictify(all_chains))
+state_map = markov_dictify(all_chains)
+
+start_chains = [seq for seq in state_map.keys() if seq[0] == START]
+
+line = list(random.choice(start_chains))
+
+def weighted_choice_sub(weights):
+    rnd = random.random() * sum(weights)
+    for i, w in enumerate(weights):
+        rnd -= w
+        if rnd < 0:
+            return i
+
+while line[-1] != END:
+    start_part = tuple(line[-2:])
+
+    next_probabilities = state_map[start_part]
+
+    weights = next_probabilities.values()
+
+    winner = weighted_choice_sub(weights)
+
+    next_part = list(next_probabilities.keys())[winner]
+
+    line.append(next_part)
+
+print(' '.join(reversed(line[1:-1])))
